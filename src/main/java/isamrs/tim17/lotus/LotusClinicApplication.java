ac1cb5490698b7a6ed328fde9ea51a623e78df7d
Mario Kujundzic <mariokipoki@gmail.com>
2020-04-04 20:07:27 +0200

    Inicijalni prazan projekat

---
+++ src/main/java/isamrs/tim17/lotus/LotusClinicApplication.java
@@ -0,0 +1,13 @@
+package isamrs.tim17.lotus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotusClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotusClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isamrs/tim17/lotus/LotusClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package isamrs.tim17.lotus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotusClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
