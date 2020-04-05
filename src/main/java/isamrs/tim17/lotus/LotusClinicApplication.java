5b011c1a91837ab204c190330dac2ab9f6938c83
Mario Kujundzic <mariokipoki@gmail.com>
2020-04-05 15:50:39 +0200

    Added patient class

--- src/main/java/isamrs/tim17/lotus/LotusClinicApplication.java
+++ src/main/java/isamrs/tim17/lotus/LotusClinicApplication.java
@@ -3,11 +3,16 @@ package isamrs.tim17.lotus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import isamrs.tim17.lotus.model.Patient;
+
 @SpringBootApplication
 public class LotusClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LotusClinicApplication.class, args);
+		
+		Patient p = new Patient("mario@gmail.com", "mario", "Mario", "Kujundzic", "Jovana Jovanovic Zmaja 4", "Subotica", "Srbija", "066 126 215", 1);
+		System.out.println(p);
 	}
 
 }
