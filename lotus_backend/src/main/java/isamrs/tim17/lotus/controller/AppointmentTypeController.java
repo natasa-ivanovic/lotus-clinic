d347f79834c32d3c2e9520cac251fd1556954808
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2020-04-28 13:18:27 +0200

    Done CRUD for appointment types on server side; table successfully generated

---
+++ lotus_backend/src/main/java/isamrs/tim17/lotus/controller/AppointmentTypeController.java
@@ -0,0 +1,79 @@
+package isamrs.tim17.lotus.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import isamrs.tim17.lotus.model.AppointmentType;
+import isamrs.tim17.lotus.service.AppointmentTypeService;
+
+@RestController
+@RequestMapping("/api")
+public class AppointmentTypeController {
+	
+	@Autowired
+	private AppointmentTypeService service;
+	
+	@PostMapping("/appointmentTypes")
+	public ResponseEntity<AppointmentType> addAppointmentType(@RequestBody String name) {
+		System.out.println("Adding an appointment type...");
+		System.out.println(name);
+		
+		AppointmentType at = new AppointmentType(name);
+		service.save(at);
+		return new ResponseEntity<>(at, HttpStatus.OK);
+	}
+	
+	@GetMapping("/appointmentTypes")
+	public ResponseEntity<List<AppointmentType>> getAllAppointmentTypes() {
+		List<AppointmentType> atypes = service.findAll();
+		return new ResponseEntity<>(atypes, HttpStatus.OK);
+	}
+	
+	@GetMapping("/appointmentTypes/{id}")
+	public ResponseEntity<AppointmentType> getAppointmentType(@PathVariable("id") long id) {
+		AppointmentType at = service.findOne(id);
+		
+		if (at == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(at, HttpStatus.OK);
+	}
+	
+	@PutMapping("/appointmentTypes/{id}")
+	public ResponseEntity<AppointmentType> updateAppointmentType(@RequestBody AppointmentType newAppointmentType, @PathVariable("id") long id) {
+		
+		if(id != newAppointmentType.getId())
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		
+		AppointmentType at = service.findOne(id);
+		
+		if (at == null)
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		
+		at.setName(newAppointmentType.getName());
+		at = service.save(at);
+		return new ResponseEntity<>(at, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("appointmentTypes/{id}")
+	public ResponseEntity<Object> deleteRoom(@PathVariable("id") long id) {
+		System.out.println(id);
+		AppointmentType at = service.findOne(id);
+		
+		if (at == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		System.out.println("Deleting " + at);
+		return new ResponseEntity<>(at, HttpStatus.OK);
+	}
+
+}
--- lotus_backend/src/main/java/isamrs/tim17/lotus/model/AppointmentType.java
+++ lotus_backend/src/main/java/isamrs/tim17/lotus/model/AppointmentType.java
@@ -5,12 +5,60 @@ package isamrs.tim17.lotus.model;
  * Purpose: Defines the Class AppointmentType
  ***********************************************************************/
 
-import java.util.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class AppointmentType {
-   private int id;
-   private String name;
-   
-   public Clinic clinic;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(name= "name", unique = false, nullable = false)
+	private String name;
+    
+	@ManyToOne(fetch = FetchType.EAGER)
+	public Clinic clinic;
+	
+	public AppointmentType() {}
+	
+	public AppointmentType(String name) {
+		this.id = this.getId();
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "AppointmentType [id=" + id + ", name=" + name + ", clinic=" + clinic + "]";
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Clinic getClinic() {
+		return clinic;
+	}
+
+	public void setClinic(Clinic clinic) {
+		this.clinic = clinic;
+	}
 
 }
