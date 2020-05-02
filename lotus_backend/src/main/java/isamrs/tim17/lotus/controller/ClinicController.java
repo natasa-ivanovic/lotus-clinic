9e2d1c3ce4a4dbcd402de1acd0ccd76dc535d0f8
VajdaBela <vajdabela01@gmail.com>
2020-05-02 16:51:30 +0200

    upgraded clinic frontend and backend

--- lotus_backend/src/main/java/isamrs/tim17/lotus/controller/ClinicController.java
+++ lotus_backend/src/main/java/isamrs/tim17/lotus/controller/ClinicController.java
@@ -6,14 +6,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import isamrs.tim17.lotus.dto.ClinicDTO;
 import isamrs.tim17.lotus.model.Clinic;
+import isamrs.tim17.lotus.model.Medicine;
 import isamrs.tim17.lotus.service.ClinicService;
 
 @RestController
@@ -34,13 +38,50 @@ public class ClinicController {
 	}
 	
 	@PostMapping("/clinics")
-	public ResponseEntity<Clinic> addClinic(@RequestBody Clinic clinic)
-	{
+	public ResponseEntity<ClinicDTO> addClinic(@RequestBody Clinic clinic) {
 		System.out.println("Adding a clinic...");
 		System.out.println(clinic);
 		
 		service.save(clinic);
 		System.out.println("Database is ok...");
-		return new ResponseEntity<>(clinic, HttpStatus.OK);
+		return new ResponseEntity<>(new ClinicDTO(clinic), HttpStatus.OK);
+	}
+	
+	@GetMapping("/clinics/{id}")
+	public ResponseEntity<ClinicDTO> getClinic(@PathVariable("id") long id) {
+		Clinic clinic = service.findOne(id);
+		
+		if (clinic == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(new ClinicDTO(clinic), HttpStatus.OK);
+	}
+	
+	@PutMapping("/clinics/{id}")
+	public ResponseEntity<ClinicDTO> updateMedicine(@RequestBody Clinic newClinic, @PathVariable long id) {
+		if(id != newClinic.getId())
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		
+		Clinic clinic = service.findOne(id);
+		
+		if(clinic == null)
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		
+		clinic.setName(newClinic.getName());
+		clinic.setAddress(newClinic.getAddress());
+		clinic.setDescription(newClinic.getDescription());
+		clinic = service.save(clinic);
+		return new ResponseEntity<>(new ClinicDTO(clinic), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/clinics/{id}")
+	public ResponseEntity<ClinicDTO> deleteRoom(@PathVariable("id") long id) {
+		System.out.println(id);
+		Clinic clinic = service.findOne(id);
+
+		if (clinic == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		System.out.println("Deleting " + clinic);
+		service.remove(id);
+		return new ResponseEntity<>(new ClinicDTO(clinic), HttpStatus.OK);
 	}
 }
