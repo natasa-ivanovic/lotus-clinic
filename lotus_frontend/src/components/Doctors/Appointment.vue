1cea47bfb81a3254facb45d201671acf62be5181
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2020-05-12 12:53:07 +0200

    Very basic page for starting appointment

--- lotus_frontend/src/components/Doctors/Appointment.vue
+++ lotus_frontend/src/components/Doctors/Appointment.vue
@@ -1,6 +1,44 @@
 <template>
     <div>
     <MedicalRecord :id="this.appointment.patientId" :edit="true"/>
+    <v-container>
+    <v-row align="center" justify="center">
+    <v-col>
+    <v-card class="elevation-3">
+        <v-toolbar flat color="secondary" dark>
+            <v-toolbar-title>Appointment type: {{appointment.type}}</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn>Report</v-btn>
+        </v-toolbar>
+    <v-card-text>
+        <v-row>
+            <v-col>
+                <v-text-field
+                    label="Diagnosis"
+                    v-model="diagnosis"/>
+            </v-col>
+            <v-col>
+                <v-text-field
+                    label="Medicine"
+                    v-model="medicine"/>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+            <v-text-field
+                label="Information"
+                v-model="info"/>
+            </v-col>
+        </v-row>
+    </v-card-text>
+    <v-card-actions>
+
+        <v-btn color="success" block>End appointment</v-btn>
+    </v-card-actions>
+    </v-card>
+    </v-col>
+    </v-row>
+    </v-container>
     </div>
 </template>
 
@@ -20,6 +58,9 @@ export default {
     },
     data() {
         return {
+            diagnosis: "",
+            medicine: "",
+            info: ""
 
         }
     },
