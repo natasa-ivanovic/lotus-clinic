fcceb61f25ebbec68118b5655d29a35e951b9842
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2020-05-31 15:23:37 +0200

    Tried to separate frontend and backend on sonarCloud

--- lotus_frontend/server.js
+++ lotus_frontend/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 //var serveStatic = require('serve-static');
 var history = require('connect-history-api-fallback');
-app = express();
+var app = express();
 
 const middleware = express.static(path.join(__dirname, 'dist'))
 
