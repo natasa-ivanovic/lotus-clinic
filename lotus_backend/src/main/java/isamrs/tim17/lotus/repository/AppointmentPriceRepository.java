90e63c2b9e2c30ab60f54f77f7da1aed8ee5780a
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2020-06-14 19:36:31 +0200

    Admins can sort appointment prices

--- lotus_backend/src/main/java/isamrs/tim17/lotus/repository/AppointmentPriceRepository.java
+++ lotus_backend/src/main/java/isamrs/tim17/lotus/repository/AppointmentPriceRepository.java
@@ -2,11 +2,15 @@ package isamrs.tim17.lotus.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import isamrs.tim17.lotus.model.AppointmentPrice;
+import isamrs.tim17.lotus.model.Clinic;
 
 public interface AppointmentPriceRepository extends JpaRepository<AppointmentPrice, Long> {
 	AppointmentPrice findOneById(long id);
 	List<AppointmentPrice> findByClinicId(Long id);
+	Page<AppointmentPrice> findAllByClinic(Clinic clinic, Pageable paging);
 }
